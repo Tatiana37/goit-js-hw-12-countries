c16da133f73c53494f2f76a2a47108ca1346788e
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-08-28 02:55:53 +0300

    HW-12 done

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import refs from './js/refs';
+import fetchCountries from './js/fetchCountries';
+import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import countryCard from './templates/countryCard.hbs';
+import countriesList from './templates/countriesList.hbs';
+// import './styles.css';
+// console.log(fetchCountries);
+
+// error({
+//   text: "I'm an error message."
+// });
+
+const debounce = require('lodash.debounce');
+
+refs.input.addEventListener('input', debounce(onInputSearch, 500));
+
+function onInputSearch(e) {
+    e.preventDefault();
+    clearInput();
+    const search = refs.input.value;
+    fetchCountries(search)
+        .then(country => {
+            if (country.length > 10) {
+                error({
+                    text: 'Too many matches found. Please enter a more specific query!'
+                });
+            } else if (country.status === 404){
+                    error({
+                    text: 'No country has been found. Please enter a more specific query!',
+                });
+            } else if (country.length === 1) {
+                renderCountryCard(country);
+            } else if (country.length <= 10) {
+                renderCountiesList(country);
+            }    
+        })
+        .catch(err => console.log(err))
+    
+}
+
+function renderCountryCard(country) {
+    return refs.details.innerHTML = countryCard(country)
+}
+
+function renderCountiesList(country) {
+    const markupList = countriesList(country);
+    refs.countriesList.insertAdjacentHTML('beforeend', markupList)
+}
+
+function clearInput() {
+    refs.details.innerHTML = '';
+    refs.countriesList.innerHTML = '';
+}
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+    input: document.querySelector('#search'),
+    countriesList: document.querySelector('.county-list'),
+    details: document.querySelector('.county-details')
+}
+
+export default refs;
