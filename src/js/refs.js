a427796086556ecdddcb1e6ffe3005fe2cc84fff
Tatiana37 <79204964+Tatiana37@users.noreply.github.com>
2021-08-28 12:06:58 +0000

    Deploying to gh-pages from @ Tatiana37/goit-js-hw-12-countries@4bba14d606944a9b83e246558392285755648eea 🚀

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-    input: document.querySelector('#search'),
-    countriesList: document.querySelector('.county-list'),
-    details: document.querySelector('.county-details')
-}
-
-export default refs;
